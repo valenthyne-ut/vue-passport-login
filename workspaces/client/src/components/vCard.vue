aa7d59b77ca0806f2e030de6d51e993126f5adfc
valenthyne-ut <valenthyne.ut@gmail.com>
2024-03-06 18:00:25 +0000

    Extract card to it's own component

---
+++ workspaces/client/src/components/vCard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="w-3/12 p-4 flex flex-col bg-white shadow-sm">
+		<slot></slot>
+	</section>
+</template>
--- workspaces/client/src/views/RootView.vue
+++ workspaces/client/src/views/RootView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+	import vCard from "@/components/vCard.vue";
 	import vButton from "@/components/vButton.vue";
 	import vField from "@/components/vField.vue";
 </script>
 
 <template>
-	<section class="w-3/12 p-4 flex flex-col bg-white shadow-sm">
+	<vCard>
 		<h1 class="text-3xl font-bold self-center">Account details</h1>
 		<ul class="pt-4 flex flex-col gap-1">
 			<li>
@@ -17,5 +18,5 @@
 		<div class="pt-1 flex">
 			<vButton class="bg-red-600 hover:bg-red-700 active:bg-red-800 text-white">Logout</vButton>
 		</div>
-	</section>
+	</vCard>
 </template>
