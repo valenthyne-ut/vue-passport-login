78f1805c8ba17ea01bf32c230567b9284b7a8f85
valenthyne-ut <valenthyne.ut@gmail.com>
2024-03-06 17:57:05 +0000

    Define account details page appearance

---
+++ workspaces/client/src/components/vField.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	import { computed } from "vue";
+
+	const props = defineProps({
+		name: {
+			type: String,
+			required: true
+		}
+	});
+
+	const cName = computed(() => {
+		return props.name.trim().endsWith(":") ? props.name : props.name + ":";
+	});
+</script>
+
+<template>
+	<div class="flex items-center">
+		<span class="py-1 px-2 me-1 rounded bg-zinc-800 text-white font-bold">{{ cName }}</span>
+		<slot></slot>
+	</div>
+</template>
--- workspaces/client/src/views/RootView.vue
+++ workspaces/client/src/views/RootView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import vButton from "@/components/vButton.vue";
+	import vField from "@/components/vField.vue";
+</script>
 
 <template>
-	<h1>Hello world!</h1>
+	<section class="w-3/12 p-4 flex flex-col bg-white shadow-sm">
+		<h1 class="text-3xl font-bold self-center">Account details</h1>
+		<ul class="pt-4 flex flex-col gap-1">
+			<li>
+				<vField name="Username"></vField>
+			</li>
+			<li>
+				<vField name="Password"></vField>
+			</li>
+		</ul>
+		<div class="pt-1 flex">
+			<vButton class="bg-red-600 hover:bg-red-700 active:bg-red-800 text-white">Logout</vButton>
+		</div>
+	</section>
 </template>
