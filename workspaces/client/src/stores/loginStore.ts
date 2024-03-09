1cf0e42270ef6a0f80500b7cf107845d877c767e
valenthyne-ut <valenthyne.ut@gmail.com>
2024-03-09 13:16:39 +0000

    Move interaction logic to LoginView component

--- workspaces/client/src/stores/loginStore.ts
+++ workspaces/client/src/stores/loginStore.ts
@@ -3,34 +3,18 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useLoginStore = defineStore("login", () => {
-	const status = ref<{ type: string, text: string }>({ type: "none", text: "" });
-
 	const username = ref<string>("");
 	const password = ref<string>("");
 
 	const userAPI = new UserAPI();
 
-	function resetStatus() {
-		setStatus("none", "");
-	}
-
-	function setStatus(type: string, text: string) {
-		status.value.type = type;
-		status.value.text = text;
-	}
-
-	async function createAccount() {
-		resetStatus();
-		if(!username.value) { return setStatus("error", "Username is required."); }
-		if(!password.value) { return setStatus("error", "Password is required."); }
+	async function attemptCreateAccount(): Promise<true> {
+		if(!username.value) { throw new Error("Username is required."); }
+		if(!password.value) { throw new Error("Password is required."); }
 		
-		try {
-			await userAPI.createUserAccount(username.value, password.value);
-			setStatus("success", "Account successfully created.");
-		} catch(error) {
-			setStatus("error", (error as Error).message || error as string);
-		}
+		await userAPI.createUserAccount(username.value, password.value);
+		return true;
 	}
 
-	return { status, username, password, resetStatus, setStatus, createAccount };
+	return { username, password, attemptCreateAccount };
 });
--- workspaces/client/src/views/LoginView.vue
+++ workspaces/client/src/views/LoginView.vue
@@ -4,21 +4,42 @@
 	import vCard from "@/components/vCard.vue";
 	import vFillButton from "@/components/vFillButton.vue";
 	import { useLoginStore } from "@/stores/loginStore";
+	import { computed, ref } from "vue";
 
 	const loginStore = useLoginStore();
+	const loginStatus = ref<{ successful: boolean, text: string }>({ successful: false, text: "" });
+
+	const cAlertType = computed(() => {
+		return loginStatus.value.successful ? "success" : "error";
+	});
+
+	function setLoginStatus(successful: boolean, text: string) {
+		loginStatus.value.successful = successful;
+		loginStatus.value.text = text;
+	}
+
+	async function createAccount() {
+		setLoginStatus(false, "");
+		try {
+			await loginStore.attemptCreateAccount();
+			setLoginStatus(true, "Account created successfully.");
+		} catch(error) {
+			setLoginStatus(false, (error as Error).message || error as string);
+		}
+	}
 </script>
 
 <template>
 	<vCard>
 		<h1 class="text-2xl font-bold self-center">Login</h1>
-		<vAlert class="mt-2" :type="loginStore.status.type" :dismissable="true" v-model:alert-text="loginStore.status.text"/>
+		<vAlert class="mt-2" :type="cAlertType" :dismissable="true" v-model:alert-text="loginStatus.text"/>
 		<LoginInputFields />
 		<div class="flex items-center gap-3 mt-4 select-none">
 			<vFillButton class="bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-xl text-white font-bold">
 				Login
 			</vFillButton>
 			or
-			<vFillButton @click="loginStore.createAccount" class="bg-green-600 hover:bg-green-700 active:bg-green-800 text-xl text-white font-bold">
+			<vFillButton @click="createAccount" class="bg-green-600 hover:bg-green-700 active:bg-green-800 text-xl text-white font-bold">
 				Create account
 			</vFillButton>
 		</div>
